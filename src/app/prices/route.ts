945f31fcd8b6fe9ae8a8208fef186e9baeb095f7
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-02-23 14:41:02 +0100

    feat: complete pricing logic

--- src/app/prices/route.ts
+++ src/app/prices/route.ts
@@ -2,47 +2,95 @@ import Moralis from "moralis";
 import { EvmChain } from "@moralisweb3/common-evm-utils";
 import { NextRequest, NextResponse } from "next/server";
 import { Frame, getFrameHtml, getFrameHtmlHead } from "frames.js";
+import fs from "fs";
 
 // Your code here
 const address = "0xAC1Bd2486aAf3B5C0fc3Fd868558b082a531B2B4";
 
 const chain = EvmChain.BASE;
-
-
+const startImageUrl = `${process.env.HOST}/images/start`;
+const initialFrame: Frame = {
+  image: startImageUrl,
+  version: "vNext",
+  buttons: [
+    {
+      label: "Calculate",
+      action: "post",
+    },
+    {
+      label: "Price Stats",
+      action: "post",
+    },
+  ],
+  inputText: "TOSHI amount. e.g 100, 30000",
+  postUrl: `${process.env.HOST}/prices`,
+  ogImage: startImageUrl,
+};
 
 export async function GET(request: NextRequest) {
-	const initialFrame: Frame = {
-    image: `${process.env.HOST}/images/start`,
-    version: "vNext",
-    buttons: [
-      {
-        label: "Work",
-        action: "post",
-      },
-    ],
-    postUrl: `${process.env.HOST}/prices`,
-  };
   return new NextResponse(
     getFrameHtml(initialFrame, { htmlBody: `<div>Hello world </div>` }),
     {
-      status: 200, headers: { "content-type": "text/html" },
+      status: 200,
+      headers: { "content-type": "text/html" },
     }
   );
 }
 export async function POST(request: NextRequest) {
-	await Moralis.start({
-		apiKey: "jBo2biMfqlNlXRSaTNSEeJwLSOHs0iToM9O97mWbWz0imjPJxaCYf2DGGHCuxrNF",
-		// ...and any other configuration
-	});
-  const response = await Moralis.EvmApi.token.getTokenPrice({
-    chain,
-    include: "percent_change",
-    address,
-  });
+  const body = await request.json();
+  const buttonId = body.untrustedData.buttonIndex;
+  const inputText = body.untrustedData.inputText;
+  const returnedFrame = { ...initialFrame };
 
-  console.log(response.toJSON());
-	return NextResponse.json(response.toJSON())
+  const inputTextAsNumber = Number(inputText);
 
+  if (!Moralis.Core.isStarted) {
+    console.log("I will start moralis");
+    await Moralis.start({
+      apiKey:
+        "jBo2biMfqlNlXRSaTNSEeJwLSOHs0iToM9O97mWbWz0imjPJxaCYf2DGGHCuxrNF",
+      // ...and any other configuration
+    });
+  }
+  try {
+    const response = await Moralis.EvmApi.token.getTokenPrice({
+      chain,
+      include: "percent_change",
+      address,
+    });
+
+    const toshiPriceData = response.result;
+    fs.writeFileSync(
+      "toshiPriceData.json",
+      JSON.stringify(toshiPriceData, null, 2)
+    );
+
+    if (response) console.log("I returned response");
+    if (buttonId == 2) {
+      const statsImageUrl = `${process.env.HOST}/image/stats?price=${toshiPriceData.usdPrice}&change=${toshiPriceData["24hrPercentChange"]}`;
+      returnedFrame.image = returnedFrame.ogImage = statsImageUrl;
+      console.log("Here's where I return the stats");
+      return new NextResponse(getFrameHtml(returnedFrame), {
+        status: 200,
+        headers: { "content-type": "text/html" },
+      });
+    } else {
+      if (isNaN(inputTextAsNumber) || inputTextAsNumber == 0) {
+        console.log("Invalid input");
+        return new NextResponse("Invalid input text", { status: 400 });
+      }
+      const amount = inputTextAsNumber * toshiPriceData.usdPrice;
+      const calculatorImageUrl = `${process.env.HOST}/image/calculator?toshi=${inputTextAsNumber}&usd=${amount}`;
+      returnedFrame.image = returnedFrame.ogImage = calculatorImageUrl;
+      console.log("Here's where I calculate the price");
+
+      return new NextResponse(getFrameHtml(returnedFrame), {
+        status: 200,
+        headers: { "content-type": "text/html" },
+      });
+    }
+  } catch (error) {
+    console.log({ error });
+    return new NextResponse("Something went wrong with api", { status: 500 });
+  }
 }
-
-
