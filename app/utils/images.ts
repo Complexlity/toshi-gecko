229b92b36bd2be8aa44f8a024a9cdb7eff0101dd
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-03-08 00:57:40 +0100

    refactor: use frog

---
+++ app/utils/images.ts
@@ -0,0 +1,14 @@
+export const imageUrls = {
+  toshi1: "https://utfs.io/f/e0aaaa7a-c6d1-4735-b2ec-a2ed8be26f3a-isneph.png",
+  toshi2: "https://utfs.io/f/a3ac9946-caf0-4073-bda1-3fcbfed96499-isnepe.png",
+  toshi3: "https://utfs.io/f/7244d7e7-9325-452c-a72a-76652c1c535a-isnepf.png",
+  toshi4: "https://utfs.io/f/1b6afe0e-e5b4-4cf3-894e-3486d81742ac-isnepg.png",
+  usFlagIcon:
+    "https://utfs.io/f/d1df2036-4bbb-4e6d-b936-7eeede0cce17-kujaph.png",
+  cryptoChart:
+    "https://utfs.io/f/a7cb2598-7c2d-4961-b35a-bb616e37dfeb-4azzfy.jpg",
+  toshiIcon:
+    "https://utfs.io/f/dd56578e-b78e-40fa-816d-830b233a7283-a79i75.png",
+  calculatorImage:
+    "https://utfs.io/f/1feb25c0-546b-4bf1-933c-223654af5561-9l0zq6.png",
+};
